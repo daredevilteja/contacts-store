e4ba65a553e3ab55f1fdbe01beb97ab0b46ff4d8
Ravi Teja Natchireddi <daredevilteja@outlook.com>
2021-08-27 22:13:58 +0530

    Added form functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'contacts-store';
+  flag = true;
+  contactList = [];
+  selectedContact = null;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    this.resetSelectedContact();
+  }
+
+  resetSelectedContact() {
+    this.selectedContact = {
+      name: "",
+      phNum: null,
+      email: ""
+    };
+  }
+
+  setFlag() {
+    this.flag = false;
+  }
+
+  saveContact() {
+    this.contactList.push(this.selectedContact);
+    this.flag = true;
+  }
+
+
+  cancel() {
+    this.resetSelectedContact();
+    this.flag = true;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DialogFormComponent } from './dialog-form/dialog-form.component';
+import {MaterialModule} from "./material/material.module";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DialogFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
