7763b0580bbb9209ae5d768c890f5f98f732bb26
Ravi Teja Natchireddi <daredevilteja@outlook.com>
2021-08-28 12:08:10 +0530

    updated form functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'contacts-store';
   flag = true;
   contactList = [];
@@ -15,11 +15,19 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    if (localStorage.getItem('all')) {
+      this.contactList = JSON.parse(localStorage.getItem('all'));
+    }
     this.resetSelectedContact();
+  }
+
+  ngOnDestroy() {
+    localStorage.clear();
   }
 
   resetSelectedContact() {
     this.selectedContact = {
+      id: null,
       name: "",
       phNum: null,
       email: ""
@@ -31,7 +39,14 @@ export class AppComponent implements OnInit {
   }
 
   saveContact() {
+    if (this.contactList[this.contactList.length] == undefined || this.contactList[this.contactList.length].id === null) {
+      this.selectedContact.id = Number(this.contactList.length) + 1;
+    } else {
+      this.selectedContact.id += 1;
+    }
     this.contactList.push(this.selectedContact);
+    localStorage.setItem('all', JSON.stringify(this.contactList));
+    this.resetSelectedContact();
     this.flag = true;
   }
 
